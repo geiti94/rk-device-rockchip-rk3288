500e9fc72b12d3e3e023e16879f1e0fd4ce0b6b5
shin_lin <Shin_Lin@asus.com>
2018-01-12 15:05:55 +0800

    ts: Add the ft5406 idc file for tinker board.
    Set the device type as touch screen.
    
    Change-Id: I1f4b8d69b1089832b42543d4039212896c03c90a

--- device.mk
+++ device.mk
@@ -77,6 +77,9 @@ PRODUCT_COPY_FILES += \
 	device/rockchip/rk3288/fstab.rk30board.bootmode.emmc.rk3288w:root/fstab.rk30board.bootmode.emmc.rk3288w
 endif
 
+# Touch
+PRODUCT_COPY_FILES += \
+	device/rockchip/rk3288/touch/fts_ts.idc:system/usr/idc/fts_ts.idc
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
